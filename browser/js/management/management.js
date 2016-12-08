abb002087795b58139c1bb63e86cf8260d361013
Ihaxor <Inderpalsdeol@gmail.com>
2016-12-07 18:37:05 -0800

    detailedCss

--- browser/js/management/management.js
+++ browser/js/management/management.js
@@ -34,16 +34,6 @@ app.controller('manageCtrl', function($scope, AuthService, projectFactory, dataF
         })
     }
 
-    // $scope.propertyName = 'table_due';
-    // $scope.reverse = true;
-
-    // $scope.sortBy = function(propertyName) {
-    //     $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-    //     $scope.propertyName = propertyName;
-    // };
-
-    // $scope.openBrowse = function(evt, tabSelection) {
-
     $scope.newProject = function() {
         var modalInstance = $modal.open({
             templateUrl: 'js/common/modals/newProject/newProject.html',
@@ -62,27 +52,4 @@ app.controller('manageCtrl', function($scope, AuthService, projectFactory, dataF
                 tableId: table_id
             })
         }
-        // $scope.openBrowse = function(evt, tabSelection) {
-    //     // Declare all variables
-    //     var i, tabcontent, tablinks;
-
-    //     // Get all elements with class="tabcontent" and hide them
-    //     tabcontent = document.getElementsByClassName("tabcontent");
-    //     for (i = 0; i < tabcontent.length; i++) {
-    //         tabcontent[i].style.display = "none";
-    //     }
-
-    //     // Get all elements with class="tablinks" and remove the class "active"
-    //     tablinks = document.getElementsByClassName("tablinks");
-    //     for (i = 0; i < tablinks.length; i++) {
-    //         tablinks[i].className = tablinks[i].className.replace(" active", "");
-    //     }
-
-    //     // Show the current tab, and add an "active" class to the link that opened the tab
-    //     document.getElementById(tabSelection).style.display = "block";
-    //     evt.currentTarget.className += " active";
-    // }
-    // document.getElementById("ProjectTab").style.display = "inline";
-    // document.getElementById("ProjectTab").className += " active";
-
 });
