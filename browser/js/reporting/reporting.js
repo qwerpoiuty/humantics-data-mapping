e9fb98432c6e22c4bd205dd722de25012a3644b8
Ihaxor <Inderpalsdeol@gmail.com>
2016-12-13 11:25:05 -0800

    frontPageDesign

--- browser/js/reporting/reporting.js
+++ browser/js/reporting/reporting.js
@@ -2,53 +2,53 @@ app.config(function($stateProvider) {
     $stateProvider.state('reporting', {
         url: '/reporting',
         templateUrl: 'js/reporting/reporting.html',
-        controller: 'reportCtrl',
-        resolve: {
-            tree: reportingFactory => {
-                let unflatten = arr => {
-                    var tree = [],
-                        mappedArr = {},
-                        arrElem,
-                        mappedElem;
+        controller: 'reportCtrl'
+        // resolve: {
+        //     tree: reportingFactory => {
+        //         let unflatten = arr => {
+        //             var tree = [],
+        //                 mappedArr = {},
+        //                 arrElem,
+        //                 mappedElem;
 
-                    // First map the nodes of the array to an object -> create a hash table.
-                    for (var i = 0, len = arr.length; i < len; i++) {
-                        arrElem = arr[i];
-                        mappedArr[arrElem.id] = arrElem;
-                        mappedArr[arrElem.id]['children'] = [];
-                    }
+        //             // First map the nodes of the array to an object -> create a hash table.
+        //             for (var i = 0, len = arr.length; i < len; i++) {
+        //                 arrElem = arr[i];
+        //                 mappedArr[arrElem.id] = arrElem;
+        //                 mappedArr[arrElem.id]['children'] = [];
+        //             }
 
 
-                    for (var id in mappedArr) {
-                        if (mappedArr.hasOwnProperty(id)) {
-                            mappedElem = mappedArr[id];
-                            // If the element is not at the root level, add it to its parent array of children.
-                            if (mappedElem.parent) {
-                                mappedArr[mappedElem['parent']]['children'].push(mappedElem);
-                            }
-                            // If the element is at the root level, add it to first level elements array.
-                            else {
-                                tree.push(mappedElem);
-                            }
-                        }
-                    }
-                    return tree;
+        //             for (var id in mappedArr) {
+        //                 if (mappedArr.hasOwnProperty(id)) {
+        //                     mappedElem = mappedArr[id];
+        //                     // If the element is not at the root level, add it to its parent array of children.
+        //                     if (mappedElem.parent) {
+        //                         mappedArr[mappedElem['parent']]['children'].push(mappedElem);
+        //                     }
+        //                     // If the element is at the root level, add it to first level elements array.
+        //                     else {
+        //                         tree.push(mappedElem);
+        //                     }
+        //                 }
+        //             }
+        //             return tree;
 
-                }
-                return reportingFactory.getTree(2).then(tree => {
-                    return unflatten(tree)
-                })
-            }
-        }
+        //         }
+        //         return reportingFactory.getTree(2).then(tree => {
+        //             return unflatten(tree)
+        //         })
+        //     }
+        // }
     })
 });
 
-app.controller('reportCtrl', function($scope, dataFactory, AuthService, reportingFactory, tree) {
+app.controller('reportCtrl', function($scope, dataFactory, AuthService, reportingFactory) {
     dataFactory.getDatabases().then(function(dbs) {
         $scope.dbs = dbs[0]
     })
 
-    $scope.tree = tree
+    // $scope.tree = tree
     $scope.impactSearches = []
     $scope.selectedDb = {}
     $scope.selectedSchema = {}
