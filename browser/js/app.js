ae57fea72269630071fec5240c39b9b9d7139c3b
Stan Le <stan.le1107@gmail.com>
2017-02-08 15:17:19 -0800

    more fixes

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,9 +1,8 @@
 'use strict';
 window.app = angular.module('data-mapping', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'ui.select']);
 
-app.config(function($urlRouterProvider, $locationProvider, $q) {
+app.config(function($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
-    $qProvider.errorOnUnhandledRejections(false);
     $locationProvider.html5Mode(true);
     // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
     $urlRouterProvider.otherwise('/');
