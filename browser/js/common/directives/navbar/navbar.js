96ba64529c90fdc5f2829ec017f9919584689ee4
Stan Le <stan.le1107@gmail.com>
2017-02-28 14:00:52 -0800

    fix

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -43,6 +43,8 @@ app.directive('navbar', function($rootScope, AuthService, AUTH_EVENTS, $state) {
                 scope.pagetitle = scope.headers[toState.name]
             })
             scope.user = null;
+
+
             scope.isLoggedIn = function() {
                 return AuthService.isAuthenticated();
             };
@@ -60,6 +62,9 @@ app.directive('navbar', function($rootScope, AuthService, AUTH_EVENTS, $state) {
                 });
             };
 
+            var removeUser = function() {
+                scope.user = null;
+            };
             scope.logout = function() {
                 AuthService.logout().then(function() {
                     $state.go('login');
