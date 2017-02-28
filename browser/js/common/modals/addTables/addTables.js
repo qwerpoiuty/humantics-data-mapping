4e6f474b9acbed61ecafb5899249e8f2b7f1b927
Stan Le <stan.le1107@gmail.com>
2017-02-28 13:26:43 -0800

    merge

--- browser/js/common/modals/addTables/addTables.js
+++ browser/js/common/modals/addTables/addTables.js
@@ -14,14 +14,16 @@ app.controller('addTableCtrl', function($scope, $uibModalInstance, project, proj
         for (const key of Object.keys($scope.tables)) {
             if ($scope.tables[key].selected == true) $scope.arr.push(Number(key))
         }
-
         $scope.arr.forEach(e => {
             if ($scope.project.tables.indexOf(Number(e)) !== -1) {
                 $scope.arr.splice($scope.arr.indexOf(Number(e)), 1)
             }
         })
+        console.log($scope.arr, $scope.project.tables)
+        $scope.arr = $scope.arr.concat($scope.project.tables)
+        console.log($scope.arr)
         projectFactory.updateProject($scope.project.project_id, "tables", $scope.arr).then(results => {
-            $uibModalInstance.close($scope.project.project_id)
+            $uibModalInstance.close($scope.arr)
         })
     }
     $scope.cancel = () => {
