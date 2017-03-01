12d2552e38951a39e76b95693edbbd6c33feaa62
Stan Le <stan.le1107@gmail.com>
2017-03-01 14:42:39 -0800

    nitin

--- browser/js/common/modals/newProject/newProject.js
+++ browser/js/common/modals/newProject/newProject.js
@@ -10,45 +10,10 @@ app.controller('newProjectCtrl', function($scope, $uibModalInstance, user, proje
     $scope.cancel = () => {
         $uibModalInstance.close()
     }
-    $scope.today = function() {
-        $scope.dt = new Date();
-    };
-    $scope.today();
+    $(function() {
+        $(".datepicker, .datepickeredit").datepicker({
+            dateFormat: 'yy/mm/dd'
+        });
+    });
 
-    $scope.open = function($event) {
-        $scope.status.opened = true;
-    };
-
-    $scope.setDate = function(year, month, day) {
-        $scope.dt = new Date(year, month, day);
-    };
-
-    $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 1
-    };
-
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
-
-    $scope.status = {
-        opened: false
-    };
-
-
-    $scope.getDayClass = function(date, mode) {
-        if (mode === 'day') {
-            var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
-
-            for (var i = 0; i < $scope.events.length; i++) {
-                var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
-
-                if (dayToCheck === currentDay) {
-                    return $scope.events[i].status;
-                }
-            }
-        }
-
-        return '';
-    };
 })
